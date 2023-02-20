cd215553da8d9e78c38d3e054b62788186d72f8a
Ingrid Echeverri <ingrid.echeverri@correounivalle.edu.co>
2023-02-20 09:43:30 -0500

    Adding repository proyect name

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-    const SERVERURL ="http://localhost/family-shop/public/";
+    const SERVERURL ="http://localhost/php-online-shop/public/";
     const APPNAME = "Family Shop";
     const MONEDA = "COP";
     date_default_timezone_set('America/Bogota');
